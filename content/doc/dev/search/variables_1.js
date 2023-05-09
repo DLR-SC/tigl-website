e54d787608bcdc2744c2ca54f368d69276a71fbc
t8ddy <t8ddy.bot@gmail.com>
2023-05-09 11:26:32 +0000

    Add newest documentation

--- content/doc/dev/search/variables_1.js
+++ content/doc/dev/search/variables_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['balance_5frounds_1954',['balance_rounds',['../structt8__profile.html#ac08fa113f773ff1c4a2f83199323dc14',1,'t8_profile']]],
-  ['balance_5fruntime_1955',['balance_runtime',['../structt8__profile.html#a50c19c111fc7fb3b8699ca09f0244115',1,'t8_profile']]]
+  ['balance_5frounds_1994',['balance_rounds',['../structt8__profile.html#ac08fa113f773ff1c4a2f83199323dc14',1,'t8_profile']]],
+  ['balance_5fruntime_1995',['balance_runtime',['../structt8__profile.html#a50c19c111fc7fb3b8699ca09f0244115',1,'t8_profile']]]
 ];
