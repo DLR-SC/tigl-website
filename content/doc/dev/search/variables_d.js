8bca3073e807d3b1b86535cf521b24171e08597f
t8ddy <t8ddy.bot@gmail.com>
2023-11-25 13:02:48 +0000

    Add newest documentation

--- content/doc/dev/search/variables_d.js
+++ content/doc/dev/search/variables_d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['orientation_2193',['orientation',['../structt8__stash__joinface.html#a419b8d7b5682cdd62dc6013dc9b24d05',1,'t8_stash_joinface']]]
+  ['orientation_2191',['orientation',['../structt8__stash__joinface.html#a419b8d7b5682cdd62dc6013dc9b24d05',1,'t8_stash_joinface']]]
 ];
