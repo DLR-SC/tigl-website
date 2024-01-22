24fcde5d73b454149d79031945b61e99548f7b0c
t8ddy <t8ddy.bot@gmail.com>
2024-01-22 08:44:14 +0000

    Add newest documentation

--- content/doc/dev/search/variables_12.js
+++ content/doc/dev/search/variables_12.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['user_5fdata_2285',['user_data',['../structt8__forest.html#a11105c5cc477058d72d0cc090a06c64a',1,'t8_forest']]],
-  ['user_5ffunction_2286',['user_function',['../structt8__forest.html#a807b7a6f5f21129753ab406993650667',1,'t8_forest']]]
+  ['user_5fdata_2289',['user_data',['../structt8__forest.html#a11105c5cc477058d72d0cc090a06c64a',1,'t8_forest']]],
+  ['user_5ffunction_2290',['user_function',['../structt8__forest.html#a807b7a6f5f21129753ab406993650667',1,'t8_forest']]]
 ];
