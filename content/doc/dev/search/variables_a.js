4755dd743ef39d827db5711525cc7f4550425133
t8ddy <t8ddy.bot@gmail.com>
2024-02-07 12:45:25 +0000

    Add newest documentation

--- content/doc/dev/search/variables_a.js
+++ content/doc/dev/search/variables_a.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['last_5fdesc_2187',['last_desc',['../structt8__tree.html#aa33e41fd2f2eb42f1aaa7452be3de289',1,'t8_tree']]],
-  ['last_5flocal_5ftree_2188',['last_local_tree',['../structt8__forest.html#a0d08dbfd69857e15bae0caaf04200aea',1,'t8_forest']]],
-  ['level_2189',['level',['../structt8__dtet.html#a8cac28ea1311c44b7cd62ce69c1446ec',1,'t8_dtet']]],
-  ['local_5fid_2190',['local_id',['../structt8__trees__glo__lo__hash__t.html#acdb8307a6bbea19a0d3f688f925a0d7e',1,'t8_trees_glo_lo_hash_t']]],
-  ['local_5fnum_5felements_2191',['local_num_elements',['../structt8__forest.html#a404bdda4711b044a3df87e21cfb3a1ab',1,'t8_forest']]]
+  ['last_5fdesc_2189',['last_desc',['../structt8__tree.html#aa33e41fd2f2eb42f1aaa7452be3de289',1,'t8_tree']]],
+  ['last_5flocal_5ftree_2190',['last_local_tree',['../structt8__forest.html#a0d08dbfd69857e15bae0caaf04200aea',1,'t8_forest']]],
+  ['level_2191',['level',['../structt8__dtet.html#a8cac28ea1311c44b7cd62ce69c1446ec',1,'t8_dtet']]],
+  ['local_5fid_2192',['local_id',['../structt8__trees__glo__lo__hash__t.html#acdb8307a6bbea19a0d3f688f925a0d7e',1,'t8_trees_glo_lo_hash_t']]],
+  ['local_5fnum_5felements_2193',['local_num_elements',['../structt8__forest.html#a404bdda4711b044a3df87e21cfb3a1ab',1,'t8_forest']]]
 ];
