c24d81b77c68403935ae7848437d30f1137c2122
t8ddy <t8ddy.bot@gmail.com>
2024-03-15 08:20:25 +0000

    Add newest documentation

--- content/doc/dev/search/functions_2.js
+++ content/doc/dev/search/functions_2.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['get_5fgeometry_1430',['get_geometry',['../structt8__geometry__handler.html#a1b24d6e38bcc4685891613f00204b055',1,'t8_geometry_handler::get_geometry(const std::string &amp;name)'],['../structt8__geometry__handler.html#a2de0ce88f684b46ba38004f7ed817e8e',1,'t8_geometry_handler::get_geometry(const size_t hash)']]],
-  ['get_5fnum_5fgeometries_1431',['get_num_geometries',['../structt8__geometry__handler.html#a4932c5f6c4665b3c16dd754ccc69094e',1,'t8_geometry_handler']]],
-  ['get_5ftree_5fgeometry_1432',['get_tree_geometry',['../structt8__geometry__handler.html#a2348e0830dee8da367e6336627655bfe',1,'t8_geometry_handler']]],
-  ['get_5ftree_5fgeometry_5ftype_1433',['get_tree_geometry_type',['../structt8__geometry__handler.html#acec6fdbce50778374bdd4be4fe007022',1,'t8_geometry_handler']]],
-  ['get_5funique_5fgeometry_1434',['get_unique_geometry',['../structt8__geometry__handler.html#a18a3973681ce624d3172417828882f00',1,'t8_geometry_handler']]]
+  ['get_5fgeometry_1431',['get_geometry',['../structt8__geometry__handler.html#a1b24d6e38bcc4685891613f00204b055',1,'t8_geometry_handler::get_geometry(const std::string &amp;name)'],['../structt8__geometry__handler.html#a2de0ce88f684b46ba38004f7ed817e8e',1,'t8_geometry_handler::get_geometry(const size_t hash)']]],
+  ['get_5fnum_5fgeometries_1432',['get_num_geometries',['../structt8__geometry__handler.html#a4932c5f6c4665b3c16dd754ccc69094e',1,'t8_geometry_handler']]],
+  ['get_5ftree_5fgeometry_1433',['get_tree_geometry',['../structt8__geometry__handler.html#a2348e0830dee8da367e6336627655bfe',1,'t8_geometry_handler']]],
+  ['get_5ftree_5fgeometry_5ftype_1434',['get_tree_geometry_type',['../structt8__geometry__handler.html#acec6fdbce50778374bdd4be4fe007022',1,'t8_geometry_handler']]],
+  ['get_5funique_5fgeometry_1435',['get_unique_geometry',['../structt8__geometry__handler.html#a18a3973681ce624d3172417828882f00',1,'t8_geometry_handler']]]
 ];
--- content/doc/dev/search/variables_b.js
+++ content/doc/dev/search/variables_b.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['last_5fdesc_2228',['last_desc',['../structt8__tree.html#aa33e41fd2f2eb42f1aaa7452be3de289',1,'t8_tree']]],
-  ['last_5flocal_5ftree_2229',['last_local_tree',['../structt8__forest.html#a0d08dbfd69857e15bae0caaf04200aea',1,'t8_forest']]],
-  ['level_2230',['level',['../structt8__dtet.html#a8cac28ea1311c44b7cd62ce69c1446ec',1,'t8_dtet']]],
-  ['local_5fid_2231',['local_id',['../structt8__trees__glo__lo__hash__t.html#acdb8307a6bbea19a0d3f688f925a0d7e',1,'t8_trees_glo_lo_hash_t']]],
-  ['local_5fnum_5felements_2232',['local_num_elements',['../structt8__forest.html#a404bdda4711b044a3df87e21cfb3a1ab',1,'t8_forest']]]
+  ['last_5fdesc_2229',['last_desc',['../structt8__tree.html#aa33e41fd2f2eb42f1aaa7452be3de289',1,'t8_tree']]],
+  ['last_5flocal_5ftree_2230',['last_local_tree',['../structt8__forest.html#a0d08dbfd69857e15bae0caaf04200aea',1,'t8_forest']]],
+  ['level_2231',['level',['../structt8__dtet.html#a8cac28ea1311c44b7cd62ce69c1446ec',1,'t8_dtet']]],
+  ['local_5fid_2232',['local_id',['../structt8__trees__glo__lo__hash__t.html#acdb8307a6bbea19a0d3f688f925a0d7e',1,'t8_trees_glo_lo_hash_t']]],
+  ['local_5fnum_5felements_2233',['local_num_elements',['../structt8__forest.html#a404bdda4711b044a3df87e21cfb3a1ab',1,'t8_forest']]]
 ];
